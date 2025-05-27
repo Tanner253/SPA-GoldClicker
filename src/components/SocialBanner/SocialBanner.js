5f0ef49690acb7ff18eb1e4406d3c76284b47dbe
SKNY <percivaltanner@gmail.com>
2025-05-27 11:49:53 -0800

    platforms

--- src/components/SocialBanner/SocialBanner.js
+++ src/components/SocialBanner/SocialBanner.js
@@ -27,14 +27,14 @@ const SocialBanner = () => {
         <span className="banner-text">🚀 Project Launch ETA: ~2 months</span>
         <span className="banner-text">Join our Community!</span>
         <div className="social-links-container">
-          <a href="https://x.com/GoldClickerSOL" target="_blank" rel="noopener noreferrer" className="social-link x-link">
-            <XIcon />
-            <span>Follow on X</span>
-          </a>
-          <a href="https://t.me/GoldClickerMining" target="_blank" rel="noopener noreferrer" className="social-link telegram-link">
-            <TelegramIcon />
-            <span>Join Telegram</span>
-          </a>
+        <a href="https://x.com/GoldClickerSOL" target="_blank" rel="noopener noreferrer" className="social-link x-link">
+          <XIcon />
+          <span>Follow on X</span>
+        </a>
+        <a href="https://t.me/GoldClickerMining" target="_blank" rel="noopener noreferrer" className="social-link telegram-link">
+          <TelegramIcon />
+          <span>Join Telegram</span>
+        </a>
           <a href="https://discord.gg/mvbx7J295m" target="_blank" rel="noopener noreferrer" className="social-link discord-link">
             <DiscordIcon />
             <span>Join Discord</span>
